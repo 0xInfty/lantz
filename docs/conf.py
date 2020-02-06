2aba142ab3a24121ccb63c872e2b945c6f248220
0xInfty <anirix.vall@gmail.com>
2020-02-06 17:56:20 -0300

    Remove Mock replacement of Numpy inside docs' conf
    I'm not sure why, but it wasn't allowing the docs to work neither locally nor online. Now it works locally, though it still returns warnings. The problematic line is commented.

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
         return Mock()
 
 MOCK_MODULES = ['numpy', ]
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+#sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
